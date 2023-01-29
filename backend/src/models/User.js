d337490739496f9f04e7b8fccded25377ab625a8
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2023-01-29 19:00:54 -0300

    criação do userModel

---
+++ backend/src/models/User.js
@@ -0,0 +1,27 @@
+const moongose = require("../database");
+
+const UserSchema = new moongose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        require: true,
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const User = moongose.model("User", UserSchema);
+
+module.exports = User;
